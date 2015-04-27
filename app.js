e32741e430a08111802f086e2a463d912e938697
Andy Sponring <asponring@gmail.com>
2015-04-27 12:30:14 -0700

    Function to create new trees

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 d3.select(".new-tree")
   .on("click", function() {
-    d3.select(this).attr("disabled", "true");
+    d3.select(this).style("display", "none");
+    createNewTree();
   });
-  
+
+var createNewTree = function(elem) {
+  d3.select(".tree-container")
+    .append("svg")
+    .attr("width", "1000")
+    .attr("height", "750");
+};
