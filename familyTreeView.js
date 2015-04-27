dafb614c3279a0b5fae29a061d408c97ca503657
Andy Sponring <asponring@gmail.com>
2015-04-27 15:52:13 -0700

    Got first node displayed on screen

--- familyTreeView.js
+++ familyTreeView.js
@@ -2,6 +2,7 @@ var FamilyTreeView = function(familyTree) {
   this.familyTree = familyTree;
   this.width = 1000;
   this.height = 750;
+  this.nodeRadius = 20;
 
 
   this.svg = d3.select(".tree-container")
@@ -13,14 +14,23 @@ var FamilyTreeView = function(familyTree) {
 };
 
 FamilyTreeView.prototype.newPerson = function() {
+  var person = new Person();
+  var personView = this.svg.append("circle")
+    .attr("r", this.nodeRadius)
+    .attr("fill", "#ff0000");
 
+  if (this.svg.size() === 1) {
+    personView.attr("cx", this.width/2)
+      .attr("cy", this.height/2);
+  }
 };
 
 FamilyTreeView.prototype.newRelationship = function() {
 
 };
 
-FamilyTreeView.prototype.askForDetails = function() {
+FamilyTreeView.prototype.askForDetails = function(detailsRequested) {
+
 
 };
 
